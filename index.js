458b537908d08b47721cff6f11f8d4e83538c51c
farazkhan2062 <farazkhan2062@gmail.com>
2024-04-01 21:33:44 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let burgur = [];
+let condition = true;
+while (condition) {
+    let order = await inquirer.prompt([
+        {
+            name: "burgur",
+            type: "input",
+            message: "What do you want in your burgur?",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more burgur?",
+            default: "false"
+        }
+    ]);
+    burgur.push(order.burgur);
+    condition = order.addMore;
+    console.log(burgur);
+}
